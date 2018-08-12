f05e3d6386e3498f7bda2f28ecb6eaa746a7b32c
yangminmin <yangminyblm@163.com>
2018-08-12 20:08:00 +0800

    feat: 新增Pug Ejs 模板演示

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const koa = require("koa");
 const app = new koa();
-const { normal } = require("./tpl");
+const { htmlTpl, ejsTpl, pugTpl } = require("./tpl");  //解构赋值的思想
+const ejs = require("ejs");
+const pug = require("pug");
 app.use(async (ctx, next) => {
 	ctx.type = "text/html;charset=utf-8";
-	ctx.body = normal;
+	ctx.body = pug.render(pugTpl, {
+		you: "Luke",
+		me: "yang"
+	});
 });
 app.listen(9999);
