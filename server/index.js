b5db5d64c22fae99bc418c7cc1a2c464ecffc9c3
yangminmin <yangminyblm@163.com>
2018-08-12 15:23:07 +0800

    feat: 新增了 HTML 静态模板文件

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const koa = require("koa");
 const app = new koa();
+const { normal } = require("./tpl");
 app.use(async (ctx, next) => {
-	ctx.body = '电影首页';
+	ctx.type = "text/html;charset=utf-8";
+	ctx.body = normal;
 });
 app.listen(9999);
