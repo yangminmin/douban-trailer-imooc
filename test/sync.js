8b61da5af388961df1a387fb25a9c6cd394bccde
yangminmin <yangminyblm@163.com>
2018-08-13 19:04:31 +0800

    feat；增加同步异步代码示例

---
+++ test/sync.js
@@ -0,0 +1,33 @@
+const doSync = (sth, time) => new Promise(resolve => {
+	setTimeout(() => {
+		console.log(sth + '用了' + time + '毫秒');
+		resolve();
+	}, time);
+})
+const doAsync = (sth, time, cb) => {
+	setTimeout(() => {
+		console.log(sth + '用了' + time + '毫秒');
+		cb && cb();
+	}, time);
+}
+const doElse = (sth) => {
+	console.log(sth);
+}
+const Scott = {doSync, doAsync};
+const Meizi = {doSync, doAsync, doElse};
+
+;(async () => {
+	console.log('case 1: 妹子来到门口');
+	await Scott.doSync('Scott刷牙', 1000);
+	console.log('一直等待，什么也不干');
+	await Meizi.doSync('妹子洗澡', 2000);
+	Meizi.doElse('妹子去忙别的了！');
+
+
+	console.log('case 3: 妹子来到门口按下通知开关');
+	Scott.doAsync('Scott 刷牙', 1000, () => {
+		console.log('Scott刷完牙了，卫生间通知妹子来洗澡');
+		Meizi.doAsync('妹子洗澡', 2000);
+	});
+	Meizi.doElse('妹子去忙别的了！');
+})()
